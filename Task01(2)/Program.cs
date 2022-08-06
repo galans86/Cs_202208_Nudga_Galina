8108b78cb47472a8707ec5757fc1bb589f0f2fc5
galina.nudga <galina.s_86@mail.ru>
2022-08-07 00:44:42 +0500

    Add Task01(2) code

--- Task01(2)/Program.cs
+++ Task01(2)/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+
+if (number1 == number2)
+{  
+Console.WriteLine($"Число {number2} равно числу {number1}"); 
+} 
+else 
+{
+ if (number1 > number2)
+ {
+  Console.WriteLine($"Число {number1} больше числа {number2}"); 
+ }
+ else
+ {
+  Console.WriteLine($"Число {number2} больше числа {number1}"); 
+ };
+};
