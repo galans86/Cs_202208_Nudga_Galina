39abec30f3b452da38762d299b23872327adea63
galina.nudga <galina.s_86@mail.ru>
2022-08-07 22:17:49 +0500

    Add Task05(6) code

--- Task05(6)/Program.cs
+++ Task05(6)/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите целое число для проверки на четность: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Четное");
+} else
+{
+   Console.WriteLine("Нечетное"); 
+};
