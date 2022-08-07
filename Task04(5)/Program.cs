dae51a9d6d8eb940bde014113974297ae26d48b9
galina.nudga <galina.s_86@mail.ru>
2022-08-07 22:56:04 +0500

    Add Task04(5) code

--- Task04(5)/Program.cs
+++ Task04(5)/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите целое число N для поиска всеx целых чисел от -N до N: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int number_step = 0 - number;
+
+while( number_step < number)
+{ 
+ Console.Write($"{number_step}, ");
+ number_step++;
+}; 
+ Console.Write($"{number_step}");
