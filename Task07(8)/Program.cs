2a8f45e4ca53e019814769aa25c3451b9ff3ad33
galina.nudga <galina.s_86@mail.ru>
2022-08-07 22:44:17 +0500

    Add Task07(8) code

--- Task07(8)/Program.cs
+++ Task07(8)/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите целое число N для поиска всеx четных чисел от 1 до N: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 2)
+{ 
+    Console.Write("Нет");
+} else
+{
+ int number_step = 2;   
+ while( number_step <= number)
+  { 
+   Console.Write($"{number_step} ");
+   number_step = number_step + 2;
+  }; 
+};
