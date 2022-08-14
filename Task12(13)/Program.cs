24510c63c6d08524b57cbe9184135ccc5151a486
galina.nudga <galina.s_86@mail.ru>
2022-08-14 22:29:10 +0500

    Add Task12(13) code

--- Task12(13)/Program.cs
+++ Task12(13)/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей цифры нет. Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int ThirdDigit(int number)
+{ 
+  int numb = number;  
+  numb = numb < 0 ? numb * -1 : numb;
+  if (numb < 100) return -1;
+  else
+  {
+    while (numb >= 1000)
+    {
+        numb = numb / 10;
+    }
+    return numb % 10;
+  }
+}
+
+Console.Write("Введите целое число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = ThirdDigit(number);
+
+if (result>=0)
+    Console.WriteLine($"Третья цифра числа {number} => {result}");
+else
+    Console.WriteLine("Третьей цифры нет");
+
+  
