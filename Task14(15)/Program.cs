62c14fe9ff4d4a2e47c1b3985e1bbaf63cba5254
galina.nudga <galina.s_86@mail.ru>
2022-08-14 22:43:51 +0500

    Add Task14(15) code

--- Task14(15)/Program.cs
+++ Task14(15)/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool Weekend(int number)
+{
+    return number <= 7 && number > 0 && ( number % 6 == 0 || number % 7 == 0 ); 
+}
+
+Console.Write("Введите цифру для проверки, является ли этот день выходным: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7 || number < 1)
+    Console.WriteLine("Ошибка ввода");
+else
+    Console.WriteLine(Weekend(number)? "Да" : "Нет");
+
