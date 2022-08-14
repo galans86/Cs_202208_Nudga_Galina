0426ea0ea016b4c8878d3b9c1b137aad2fb7ae2d
galina.nudga <galina.s_86@mail.ru>
2022-08-14 22:03:18 +0500

    Add Task09(10) code

--- Task09(10)/Program.cs
+++ Task09(10)/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int SecondDigit(int numb)
+{
+    return ( numb / 10 ) % 10;
+}
+
+int number = new Random().Next(1, 1000);
+Console.WriteLine($"Вторая цифра числа {number} => {SecondDigit(number)}");
