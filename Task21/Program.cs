a0e594014278a9452d7b279895c8bf708c5453f4
galina.nudga <galina.s_86@mail.ru>
2022-08-15 20:56:33 +0500

    Add Task21 code

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -3,7 +3,7 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-double Destination(int xc1,int yc1, int zc1, int xc2, int yc2, int zc2)
+double Distance(int xc1,int yc1, int zc1, int xc2, int yc2, int zc2)
 {
     return Math.Sqrt( Math.Pow(xc2-xc1,2) + Math.Pow(yc2-yc1,2) + Math.Pow(zc2-zc1,2));
 }
@@ -24,5 +24,5 @@ int yc2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("z = ");
 int zc2 = Convert.ToInt32(Console.ReadLine());
 
-double Dest = Math.Round( Destination(xc1,yc1,zc1,xc2,yc2,zc2), 2 );
-Console.WriteLine($"Расстояние между точками А({xc1},{yc1},{zc1}) и B({xc2},{yc2},{zc2}) = {Dest}");
+double Dist = Math.Round( Distance(xc1,yc1,zc1,xc2,yc2,zc2), 2 );
+Console.WriteLine($"Расстояние между точками А({xc1},{yc1},{zc1}) и B({xc2},{yc2},{zc2}) = {Dist}");
