0933290aa51c8c7c30955123d623a32a94674d96
galina.nudga <galina.s_86@mail.ru>
2022-08-17 21:59:58 +0500

    Add Task29 code

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] FillArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for( int i =0; i < array.Length; i++)
+        array[i] = new Random().Next(min, max);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+     for( int i =0; i < array.Length - 1; i++)
+         Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}] ");
+}
+
+int[] array = FillArray(8,0,100);
+PrintArray(array);
+Console.WriteLine();
