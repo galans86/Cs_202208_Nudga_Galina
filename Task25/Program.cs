c9588531190714a8ac44fba721e94be7061624dd
galina.nudga <galina.s_86@mail.ru>
2022-08-17 21:20:03 +0500

    Add Task25 code

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Degree( int number, int degree)
+{
+    int result = 1;
+    for( int i = 0; i < degree; i++)
+        result *= number;
+
+    return result;
+}
+
+Console.WriteLine("Введите число А: ");
+int ac = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральную степень B: ");
+int bc = Convert.ToInt32(Console.ReadLine());
+
+if (bc < 0) 
+    Console.WriteLine("Ошибка ввода");
+else
+{
+    int result = Degree(ac,bc);
+    Console.WriteLine($"А в степени В равно {result}");
+}
