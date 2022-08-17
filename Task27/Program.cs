fa4423739a8519bf9d0b8db7c1681b2ded53a61c
galina.nudga <galina.s_86@mail.ru>
2022-08-17 21:43:55 +0500

    Add Task27 code

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int DigitSum(int number)
+{
+   int result = 0;
+   while(number > 10)
+   {
+     result += number % 10;
+     number /= 10; 
+   }     
+    return result += number;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) 
+    Console.WriteLine("Ошибка ввода");
+else
+{ 
+    int result = DigitSum( number );
+    Console.WriteLine($"Сумма цифр числа {number} равна {result}");
+}
