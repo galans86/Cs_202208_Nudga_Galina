36cea6372d0b4be196d3f9ab388af39fdbed75d8
galina.nudga <galina.s_86@mail.ru>
2022-08-21 11:01:54 +0500

    Task38 pause

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateRndDouble(int size, int max)
+{
+    double[] array = new double[size];
+    Random randObj = new Random();
+    double number = 0;
+    for(int i=0; i<size; i++)
+    {
+      number = randObj.NextDouble()*max;
+      array[i] = Math.Floor(number);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+     for( int i =0; i < array.Length - 1; i++)
+         Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}] ");
+}
+
+
+double[] array = CreateRndDouble(4,0,10);
+PrintArray(array);
+Console.WriteLine();
