edefb48af7a7052a4f5dab09d8c82575341801a7
galina.nudga <galina.s_86@mail.ru>
2022-08-21 10:41:01 +0500

    Add Task36 code

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -3,3 +3,33 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] CreateArrRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for( int i=0; i < array.Length; i++)
+        array[i] = rnd.Next(min, max+1);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+     for( int i =0; i < array.Length - 1; i++)
+         Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}] ");
+}
+
+int SumOddPos(int[] array)
+{
+    int sum = 0;
+    for( int i=0; i < array.Length; i++)  
+    {
+        if (i % 2 != 0) sum += array[i];
+    }
+    return sum;
+}
+int[] array = CreateArrRndInt(4,0,99);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях в массиве = {SumOddPos(array)}");
