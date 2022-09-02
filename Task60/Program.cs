1fea33742f51322343bcb64eaa327486197ac019
galina.nudga <galina.s_86@mail.ru>
2022-09-03 00:56:51 +0500

    Add Task60 code

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+int[,,] CreateDeepArray(int row, int col, int deep)
+{
+    int[,,] deepArray = new int[row, col, deep];
+    int number = 10;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            for (int k = 0; k < deep; k++)
+            {
+                deepArray[i, j, k] = number;
+                number++;
+                if (number >= 100) number = 10;
+            }
+        }
+    }
+    return deepArray;
+}
+
+
+void PrintDeepArray(int[,,] deepArray)
+{
+    for (int k = 0; k < deepArray.GetLength(2); k++)
+    {
+        for (int i = 0; i < deepArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < deepArray.GetLength(1); j++)
+            {
+                Console.Write($"{deepArray[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+int[,,] deepArray = CreateDeepArray(2, 2, 2);
+PrintDeepArray(deepArray);
