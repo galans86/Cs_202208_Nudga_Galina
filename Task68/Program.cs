e4268957083c6bb42cbbe1cd6d221cef01a11579
galina.nudga <galina.s_86@mail.ru>
2022-09-09 19:58:57 +0500

    Add Task68 code

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 3 n = 2 -> A(m,n) = 29
+
+Console.Write("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 0 || n < 0)
+    Console.WriteLine("Ошибка ввода!");
+else
+    Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
+
+long Akkerman(long m, long n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0 && m > 0)
+        return Akkerman(m - 1, 1);
+    else
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+}
